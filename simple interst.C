a7baefd1459b8e8ff8b6edb0950aa2f33e2b28b7
dheeraj192006 <dheeraj992006@gmail.com>
2024-11-26 12:28:22 +0530

    Create simple interst.C

---
+++ simple interst.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Enter the value of principal,rate and time\n");
+  float p,r,t,si;
+  scanf("%f%f%f",&p,&#ir,&t);
+  si=(p*r*t)/100;
+  printf("The simple interest is %f",si);
+  return 0;
+}
